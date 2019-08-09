07b5da21c5d8a573393d510a3660a1180a35609f
Rodrigo Berlochi <rodrigo.berlochi@endava.com>
2019-08-09 00:25:44 -0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const webpack = require('webpack');
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "react-boxes-selector.js",
+    library: "reactBoxesSelector",
+    libraryTarget: "commonjs2"
+  },
+  externals: {
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "react",
+      root: "React"
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/react"]
+          }
+        }
+      }
+    ]
+  }
+};
